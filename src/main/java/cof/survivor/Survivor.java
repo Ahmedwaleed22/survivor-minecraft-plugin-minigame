fdb7acabd355fc451ff8ffafde0c54ce19e04712
Ahmed Waleed <ahmedwaleedaboelamayem68@gmail.com>
2022-05-02 11:59:57 +0200

    Inital Commit

---
+++ src/main/java/cof/survivor/Survivor.java
@@ -0,0 +1,17 @@
+package cof.survivor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Survivor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
